76a3f0b74aeacac982890c9d098941916efc93cb
Pourbaix Michaël <mi.pourbaix@students.ephec.be>
2023-03-07 18:56:18 +0100

    feat(security): Fixing security problem with the express-session secret

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const session = require("express-session");
 // const AutoImportData = require("./models/modelAutoImportData");
+const Insulin = require("./models/modelInsulin");
 const autoImportData = require("./autoImportData.js");
 
 require("dotenv").config();
@@ -18,7 +19,8 @@ app.use(cors(corsOptions));
 ////////////////// Passport //////////////////////////
 const passport = require("./config/passport");
 
-app.use(session({ secret: "test" }));
+// app.use(session({ secret: "test" }));
+app.use(session({ secret: process.env.SESSION_SECRET }));
 app.use(passport.initialize({ session: false }));
 app.use(passport.session({ session: false }));
 
@@ -44,6 +46,17 @@ seq.sequelize
 		console.log("An error occurred while Synchronization.\n", error)
 	);
 // AutoImportData.sync();
+// Insulin.drop();
+// Insulin.sync({ alter: true });
+
+// seq.sequelize
+// 	.query("drop table bolus", {
+// 		type: seq.Sequelize.QueryTypes.DROP,
+// 	})
+// 	.then((result) => {
+// 		console.log(result);
+// 	});
+
 //////////////////////////////////////////////////////
 
 ///////////////// Route modules //////////////////////
@@ -51,19 +64,19 @@ const routeUser = require("./routes/routeUser");
 const routeTag = require("./routes/routeTag");
 const routeData = require("./routes/routeData");
 const routeDetectionRanges = require("./routes/routeDetectionRanges");
-const routeBolus = require("./routes/routeBolus");
+const routeInsulin = require("./routes/routeInsulin");
 
 app.use("/api/tags", routeTag);
 app.use("/api/users", routeUser);
 app.use("/api/data", routeData);
 app.use("/api/ranges", routeDetectionRanges);
-app.use("/api/bolus", routeBolus);
+app.use("/api/insulin", routeInsulin);
 //////////////////////////////////////////////////////
 
 app.listen(3001);
 console.log("App listening on port 3001!");
 
-/////////////////auto Import//////////////////////////
+/////////////////Auto Import//////////////////////////
 // Execute the import script every 12 hours //
 
 const cron = require("node-cron");
